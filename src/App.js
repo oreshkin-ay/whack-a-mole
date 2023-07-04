cae447a945ce4a61c4e4432fd186ddb6bac2cb11
a.oreshkin <a.oreshkin@corp.mail.ru>
2023-07-04 07:28:46 +0300

    game

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React12121
         </a>
       </header>
     </div>
